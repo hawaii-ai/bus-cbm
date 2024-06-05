897f10fce92671a95fe9da9e408f94687545d173
Arianna Bunnell <88562918+AriBunnell@users.noreply.github.com>
2024-06-05 13:51:07 -1000

    Update __init__.py
    adding additional exports

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,3 +2,5 @@ from .cancer_head_fpn_conv import build_cancer_head, CBMCancerHead
 from .evaluator import CBMCOCOEvaluator
 from .mapper import ValidationMapper, CustomMapper
 from .cbm_roi_heads_fpn import CBMStandardROIHeads
+from .config import add_cbm_config, add_uhcc_config
+from .visualizer import MyVisualizer 
